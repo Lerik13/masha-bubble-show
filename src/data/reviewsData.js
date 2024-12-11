8accaf93042a611972600f254408ad5458b46b89
Valeria <lerik13@gmail.com>
2024-12-11 12:15:17 -0700

    + section 'Review Carousel'

---
+++ src/data/reviewsData.js
@@ -0,0 +1,46 @@
+const reviewsData = [
+	{
+		"image": "/reviews/1.jpg",
+		"text": `We want to share our incredible experience with Maria. Her shows are breathtaking for everyone! Whether it's a kid or an adult, everyone watches with joy and a smile. She did a personal show for my daughter's birthday a few years ago, and just recently, we attended her New Year Bubble show. It was a memorable event. My son, who is 12, thoroughly enjoyed. Even my husband, who usually looks at his phone, watched the entire show, captivated by Maria's magic. "Wow Bear" is a must-have addition to every event!`,
+		"name": "Ksenia Mueller",
+		"link": "https://www.facebook.com/ksenia.mueller.9",
+		"rating": 5,
+	},
+	{
+		"image": "/reviews/2.jpg",
+		"text": "Maria's Bubble Show is a true gem! If you're looking for an entertainment experience that everyone will enjoy, Maria is the go-to choice. Working with Maria is an absolute pleasure. She's incredibly accommodating and can adapt to any unique requests or nuances you might have.",
+		"name": "Natalia Kutsova",
+		"link": "https://www.facebook.com/natalia.kutsova",
+		"rating": 5,
+	},
+	{
+		"image": "/reviews/3.jpg",
+		"text": "The show we had with Maria was nothing short of amazing. From the very start to the grand finale, our guests were captivated, delighted, and left thoroughly satisfied. Maria's bubbly personality truly shines through in her performance, making it an unforgettable and joyous experience for all. Don't miss out on this incredible show!",
+		"name": "Alina Avena",
+		"link": "https://www.facebook.com/profile.php?id=100082520804786",
+		"rating": 5,
+	},
+	{
+		"image": "/reviews/4.jpg",
+		"text": `First I saw Maria's bubble show at friends birthday party. Kids were amazed, actually adults too. There are so many ways what "magic" Maria can do with bubbles.<br> And the most important our daughter stayed happy and satisfied. Thank you, Maria. You are doing great show!`,
+		"name": "Vera Tikhonova ",
+		"link": "https://www.facebook.com/vera.tikhonova.7",
+		"rating": 5,
+	},
+	{
+		"image": "/reviews/1.jpg",
+		"text": "Maria did a show for my daughter's 5th birthday and everyone absolutely loved it! Her show isn't just for kids, adults enjoyed it very much as well. I've had a lot of various parties for my kids over the years but Maria's party by far is the best. She truly loves what she does and it shows. She engaged the kids and parents the whole time. She has some cool tricks up her sleeve and the bubbles you'll see range from tinies to gigantic. It's truly not something I've ever seen before.",
+		"name": "Maria Chaliy",
+		"link": "https://www.facebook.com/mariachaliy",
+		"rating": 5,
+	},
+	{
+		"image": "/reviews/1.jpg",
+		"text": "Prepare to be mesmerized! Maria's bubble show was an absolute delight for both kids and adults alike.From the moment the show started, I was captivated by the intricate and colorful bubbles floating gracefully through the air. Maria is a very talented performer! Whether you're a kid or just a kid at heart, this show will leave you awe-struck and smiling from ear to ear.",
+		"name": "Jocelyn Alexander",
+		"link": "https://www.instagram.com/jocelynalexander2.0",
+		"rating": 5,
+	},
+]
+
+export default reviewsData;
