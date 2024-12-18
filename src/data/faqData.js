53245a1f19e326904270de82f3f3a2c71cd0690e
Valeria <lerik13@gmail.com>
2024-12-17 17:18:13 -0700

    + section 'FAQ'

---
+++ src/data/faqData.js
@@ -0,0 +1,78 @@
+const faqData = [
+	{
+		"id": 1,
+		"question": "HOW TO BOOK YOUR SHOW?",
+		"answer": `Select the one that best suits your event from the list of show options.
+\n
+Go to the "Book Your Show" button and fill out the request form. Please provide the date, time, and address of the show. This information will help us understand if we have availability on your date/ time.
+\n
+After filling out the form, we will contact you to confirm details.
+\n
+Reservations are made only after a 50% deposit is received. Before that the date/time is considered available.
+\n
+Due to limited availability, we cannot hold spots unless they are reserved.`
+	},
+	{
+		"id": 2,
+		"question": "WHAT HAPPENS TO THE DEPOSIT IF I CANCEL/ RESCHEDULE THE EVENT?",
+		"answer": "Our 50% deposit is non-refundable, but we understand that things happen. If your plans change, we can reschedule your show for another available date. Please inform us as soon as possible, but no later than 2 days before the confirmed date."
+	},
+	{
+		"id": 3,
+		"question": "CAN I MAKE CHANGES TO MY ALREADY BOOKED?",
+		"answer": "Yes, we are always open to discussing and trying to accommodate your requests. We strive to make your event the best experience you've ever had."
+	},
+	{
+		"id": 4,
+		"question": "DOES THE FLOOR GETS MESSY FROM THE BUBBLE SOLUTION?",
+		"answer": "No! We use special heavy- duty waterproof rugs. Everything is left clean after the show."
+	},
+	{
+		"id": 5,
+		"question": "HOW MUCH SPACE TO YOU NEED FOR THE SHOW?",
+		"answer": `Indoors we cover 12x15 feet space with waterproof rugs.
+\n
+In general, the more space you have, the better, however we can perform in a smaller area (6x7 ft).`
+	},
+	{
+		"id": 6,
+		"question": "HOW MUCH TIME DO YOU NEED TO SET UP/PUT AWAY EQUIPMENT?",
+		"answer": `We usually arrive 50-60 minutes before an indoor show and 40-50 minutes before an outdoor show.
+\n
+Let us know if you need our performance area and equipment to be set up in advance, before your guests arrive.`
+	},
+	{
+		"id": 7,
+		"question": "HOW TO PREPARE FOR THE SHOW?",
+		"answer": `INDOOR:
+\n
+1. It’s important that children remain seated during the show.\n
+2. Arrange seating for the audience in advance.\n
+3.  Please avoid using artificial scents in the room.\n
+4. The AC should be turned off during the show.\n
+5. We will need an outlet \n
+\n
+OUTDOOR:
+\n
+ 1. Please remove any objects from the space.\n
+ 2. Check for a nearby outlet to ensure it is working (I have an extension cord if needed).`
+	},
+	{
+		"id": 8,
+		"question": "CAN I MOVE MY BOOKED SHOW FROM OUTDOORS TO INDOORS DUE TO BAD WEATHER?",
+		"answer": `Yes, we would be happy to accommodate your request. Please monitor the weather forecast closely and inform us at least 6 hours before showtime so we can bring the appropriate equipment. `
+	},
+	{
+		"id": 9,
+		"question": "HOW MANY KIDS CAN PARTICIPATE IN THE SHOW?",
+		"answer": `We recommend limiting outdoor shows to no more than 25 children to ensure that everyone has the best experience. 
+If you plan to have more kids, it would be better to divide them into groups or hold the show indoors.`
+	},
+	{
+		"id": 10,
+		"question": "ARE YOU INSURED?",
+		"answer": `Yes, our company is fully insured and we can add event's venue to our COl (Certificate of insurance)`
+	},
+];
+
+export default faqData;
